8777b46fbd31a6914ee7fd3a158887625d76b67e
brun58 <brun58@ya.ru>
2021-11-09 19:56:53 +0300

    Basic project structure

---
+++ src/database/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.util.List;
+
+public class DatabaseHelper {
+    // создаем необходимые таблицы
+    public void initDatabase() {
+
+    }
+
+    // добавляем новый канал в базу
+    public void saveChannel() {
+
+    }
+
+    // добавляем все новости в базу
+    public void saveNews() {
+
+    }
+
+    // достаем из базы все сохраненные новости
+    public List<Object> getAllNews() {
+        return null;
+    }
+
+    // достаем из базы все каналы
+    public List<Object> getAllChannels() {
+        return null;
+    }
+}
---
+++ src/xml/XmlParser.java
@@ -0,0 +1,10 @@
+package xml;
+
+import java.util.List;
+
+public class XmlParser {
+    // Unirest - get request
+    public List<Object> fetchAndParseRss(String url) {
+        return null;
+    }
+}
