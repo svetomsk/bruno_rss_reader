82c9f7e978f1547be2fd459d34f7c26cabe8920a
2XCommandeRX5 <yuriy.vinogradov@jetbrains.com>
2021-11-09 21:28:29 +0300

    Сделал домПарсер

--- src/xml/ReadXmlDomParser.java
+++ src/xml/ReadXmlDomParser.java
@@ -10,49 +10,52 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadXmlDomParser
 {
-        public void rssParseTables()
-        {
+    public List<newsItem> tempArray = new ArrayList<>();
 
-            // Instantiate the Factory
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    public List<newsItem> rssParseTables()
+    {
+        // Instantiate the Factory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
-            try {
+        try {
 
-                // parse XML file
-                DocumentBuilder db = dbf.newDocumentBuilder();
-                Document doc = db.parse("https://www.sport.ru/rssfeeds/football.rss");
+            // parse XML file
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse("https://www.sport.ru/rssfeeds/football.rss");
 
-                doc.getDocumentElement().normalize();
+            doc.getDocumentElement().normalize();
 
-                // get <channel>
-                NodeList list = doc.getElementsByTagName("item");
+            // get <channel>
+            NodeList list = doc.getElementsByTagName("item");
 
-                for (int temp = 0; temp < list.getLength(); temp++)
-                {
-                    Node node = list.item(temp);
-                    if (node.getNodeType() == Node.ELEMENT_NODE) {
-                        Element element = (Element) node;
-                        // get text
-                        String title = element.getElementsByTagName("title").item(0).getTextContent();
-                        String link = element.getElementsByTagName("link").item(0).getTextContent();
-                        String description = element.getElementsByTagName("description").item(0).getTextContent();
-                        String pubDate = element.getElementsByTagName("pubDate").item(0).getTextContent();
+            for (int temp = 0; temp < list.getLength(); temp++) {
+                Node node = list.item(temp);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    // get text
+                    String title = element.getElementsByTagName("title").item(0).getTextContent();
+                    String link = element.getElementsByTagName("link").item(0).getTextContent();
+                    String description = element.getElementsByTagName("description").item(0).getTextContent();
+                    String pubDate = element.getElementsByTagName("pubDate").item(0).getTextContent();
 
-                        System.out.println("Title : " + title);
-                        System.out.println("Link : " + link);
-                        System.out.println("Description : " + description);
-                        System.out.println("Publication date : " + pubDate);
-                    }
+                    System.out.println("Title : " + title);
+                    System.out.println("Link : " + link);
+                    System.out.println("Description : " + description);
+                    System.out.println("Publication date : " + pubDate);
+                    tempArray.add(new newsItem(title, link, description, pubDate));
                 }
-
             }
+            return tempArray;
 
-            catch (ParserConfigurationException | SAXException | IOException e)
+            } catch (ParserConfigurationException | SAXException | IOException e)
             {
                 e.printStackTrace();
             }
-        }
+            return null;
     }
+}
---
+++ src/xml/newsItem.java
@@ -0,0 +1,18 @@
+package xml;
+
+public class newsItem
+{
+    private String title;
+    private String link;
+    private String description;
+    private String pubDate;
+
+    public newsItem(String title, String link, String description, String pubDate)
+    {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubDate = pubDate;
+    }
+
+}
