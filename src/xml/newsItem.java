380fcb0c10772304f2664ba189f107b2dd179d80
2XCommandeRX5 <yuriy.vinogradov@jetbrains.com>
2021-11-09 21:32:18 +0300

    Сделал домПарсер

--- src/xml/newsItem.java
+++ src/xml/newsItem.java
@@ -7,6 +7,22 @@ public class newsItem
     private String description;
     private String pubDate;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
     public newsItem(String title, String link, String description, String pubDate)
     {
         this.title = title;
